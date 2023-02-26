84b34f70276d9ede151a3f04a91db18914104c77
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-26 22:02:22 +0100

    Cleanup base project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+import { setDefaultResultOrder } from 'dns'
+
+setDefaultResultOrder('verbatim')
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+    plugins: [sveltekit()],
+})
