dee12931be49faed7a2e7e595f4967ae7141792d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-27 18:28:26 +0100

    Use https for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { setDefaultResultOrder } from 'dns'
 
 setDefaultResultOrder('verbatim')
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), basicSsl()],
 })
