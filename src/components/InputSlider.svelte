620ac9bc54ecd40b48c3d1be0f36f009d97d38f9
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-27 07:18:50 +0100

    Fix styling in webkit browsers

--- src/components/InputSlider.svelte
+++ src/components/InputSlider.svelte
@@ -67,7 +67,7 @@
         transition: background 0.3s ease-in-out;
     }
 
-    input[type='range']::-moz-range-thumb {
+    input[type='range']::-webkit-slider-thumb {
         appearance: none;
         box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
         height: var(--thumb-size);
