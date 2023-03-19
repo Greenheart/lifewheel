6bad0dc0a53c60b2dcfe265505c0eb416fa6f05c
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-03-19 12:36:08 +0100

    Add hover state to outline buttons

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,10 +3,12 @@
 
     export const variants = {
         solid: 'rounded-lg bg-gradient-to-br from-emerald-400 to-emerald-400/75 px-3 py-2 text-lg text-black hover:from-emerald-500 hover:to-emerald-500/75',
-        outline: 'rounded-lg px-3 py-2 text-lg hover:from-emerald-500 hover:to-emerald-500/75',
+        outline:
+            'rounded-lg px-3 py-2 text-lg hover:from-emerald-500 hover:bg-emerald-400/10 hover:to-emerald-500/75',
         roundSolid:
             'bg-gradient-to-br from-emerald-400 to-emerald-400/75 hover:from-emerald-500 hover:to-emerald-500/75 w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 text-2xl rounded-full text-black',
-        roundOutline: 'w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 text-2xl rounded-full',
+        roundOutline:
+            'w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 text-2xl rounded-full hover:bg-emerald-400/10',
     }
     export const defaultVariant = 'solid'
     export const defaultClasses = 'border-2 border-emerald-400 hover:border-emerald-500 shadow-md'
