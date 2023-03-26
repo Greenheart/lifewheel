9a02125e5454707f2aa4a34baeb6d0efa2153625
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-03-26 19:38:54 +0200

    WIP improve tabs

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -5,13 +5,13 @@
     export const variants = {
         solid: 'rounded-lg bg-gradient-to-br from-emerald-400 to-emerald-400/75 px-3 py-2 text-lg text-black hover:from-emerald-500 hover:to-emerald-500/75',
         outline: 'rounded-lg px-3 py-2 text-lg hover:bg-emerald-400/10',
-        roundSolid:
-            'bg-gradient-to-br from-emerald-400 to-emerald-400/75 hover:from-emerald-500 hover:to-emerald-500/75 w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 text-2xl rounded-full text-black',
-        roundOutline:
-            'w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 text-2xl rounded-full hover:bg-emerald-400/10',
         ghost: 'rounded-lg px-3 py-2 text-lg hover:bg-emerald-400/5 border-transparent hover:border-emerald-500/5 shadow-none',
+        roundSolid:
+            'bg-gradient-to-br from-emerald-400 to-emerald-400/75 hover:from-emerald-500 hover:to-emerald-500/75 w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 text-2xl rounded-full text-black grid place-items-center',
+        roundOutline:
+            'w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 text-2xl rounded-full hover:bg-emerald-400/10 grid place-items-center',
         roundGhost:
-            'rounded-full text-lg w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 hover:bg-emerald-400/5 border-transparent hover:border-emerald-500/5 shadow-none',
+            'rounded-full text-lg w-12 2xs:w-14 xs:w-16 h-12 2xs:h-14 xs:h-16 hover:bg-emerald-400/5 border-transparent hover:border-emerald-500/5 shadow-none grid place-items-center',
     }
     export const defaultVariant = 'solid'
     export const defaultClasses = 'border-2 border-emerald-400 hover:border-emerald-500 shadow-md'
--- src/components/ManageData.svelte
+++ src/components/ManageData.svelte
@@ -8,23 +8,58 @@
     import FolderOpen from '$icons/FolderOpen.svelte'
 
     import { openFile } from '$lib/import'
-    import { saveFile } from '$lib/export'
+    import { encodeReflectionEntries, formatLink, saveFile, showQRCode } from '$lib/export'
 </script>
 
 <script lang="ts">
     import { reflections, loading } from '$lib/stores'
     import { cx } from '$lib/utils'
-    import CloseButton from './CloseButton.svelte'
+    import Link from '$icons/Link.svelte'
+    import Close from '$icons/Close.svelte'
 
     let expanded = false
+
+    let canvas: HTMLCanvasElement
+    let isQRReady = false
+
+    let copyText = 'Copy your link'
+
+    const copyLink = async (hash: string) => {
+        copyText = 'Copied!'
+
+        const url = new URL(window.location.origin)
+        url.hash = hash
+
+        await showQRCode(url.toString(), canvas)
+            .then(() => {
+                isQRReady = true
+            })
+            .catch((error) => console.error(error))
+
+        // Clipboard is only available in via HTTPS or localhost
+        await navigator?.clipboard?.writeText(url.toString())
+
+        window.setTimeout(() => {
+            copyText = 'Copy your link'
+        }, 2000)
+    }
 </script>
 
 <div class="mx-auto w-full max-w-md pt-16" class:invisible={$loading}>
-    <!-- IDEA: Maybe always show both the open and save buttons -->
     <TabGroup class="manage-data">
-        <TabList>
-            <Tab class={tabClasses} on:click={() => (expanded = true)}>Open</Tab>
-            <Tab class={tabClasses} on:click={() => (expanded = true)}>Save</Tab>
+        <TabList class="flex justify-center">
+            <Tab
+                class={cx(tabClasses, 'inline-flex items-center gap-2')}
+                on:click={() => (expanded = true)}><FolderOpen />Open</Tab
+            >
+            <Tab
+                class={cx(tabClasses, 'inline-flex items-center gap-2')}
+                on:click={() => (expanded = true)}><Download />Save</Tab
+            >
+            <Tab
+                class={cx(tabClasses, 'inline-flex items-center gap-2')}
+                on:click={() => (expanded = true)}><Link />Link</Tab
+            >
         </TabList>
         <TabPanels
             class={cx('relative mt-2 rounded-md bg-gray-50/5 p-4', expanded ? undefined : 'hidden')}
@@ -32,16 +67,16 @@
             <TabPanel>
                 <Button
                     variant="roundGhost"
-                    class="absolute right-4 top-4 flex !h-12 !w-12 items-center justify-center !border-emerald-400/5 pb-1 !text-xs !shadow-md"
-                    on:click={() => (expanded = false)}><span>⨉</span></Button
+                    class="absolute right-4 top-4 !h-12 !w-12 !border-emerald-400/5"
+                    on:click={() => (expanded = false)}><Close /></Button
                 >
-
-                <p class="pb-4">Load your data from a file.</p>
                 <!-- TODO: support opening multiple files, and automatically combine into single state. Also if one of the files fail to load, handle that error so other files can still be loaded -->
                 <!-- TODO: Limit to only accept json files -->
                 <Button variant="ghost" on:click={openFile} class="flex items-center gap-2"
-                    ><FolderOpen />Open</Button
+                    ><FolderOpen />Open file</Button
                 >
+                <p class="pt-4">Load your data from a file.</p>
+
                 <h2 class="pt-4 text-lg font-bold">Tips</h2>
                 <p class="pt-2">
                     Are you using this app on multiple devices? You can open multiple files to
@@ -52,46 +87,37 @@
             <TabPanel>
                 <Button
                     variant="roundGhost"
-                    class="absolute right-4 top-4 flex !h-12 !w-12 items-center justify-center !border-emerald-400/5 pb-1 !text-xs !shadow-md"
-                    on:click={() => (expanded = false)}><span>⨉</span></Button
+                    class="absolute right-4 top-4 !h-12 !w-12 !border-emerald-400/5"
+                    on:click={() => (expanded = false)}><Close /></Button
                 >
 
                 <Button
                     on:click={() => saveFile($reflections)}
                     variant="ghost"
-                    class="flex items-center gap-2"><Download />Save</Button
+                    class="flex items-center gap-2"><Download />Save file</Button
                 >
+            </TabPanel>
+            <TabPanel>
+                <Button
+                    variant="roundGhost"
+                    class="absolute right-4 top-4 !h-12 !w-12 !border-emerald-400/5"
+                    on:click={() => (expanded = false)}><Close /></Button
+                >
+                <!-- TODO: describe how the link works -->
+                <!-- TODO: Add switch to enable/disable encryption. Then use that state to generate the link + QR code -->
+                <!-- TODO: preserve QR code state when tab opens/closes -->
+
+                <Button
+                    on:click={() =>
+                        copyLink(formatLink({ data: encodeReflectionEntries($reflections) }))}
+                    variant="ghost"
+                    class="flex items-center gap-2"><Link />Copy link</Button
+                >
+                <div class="pt-4" class:hidden={!isQRReady}>
+                    <h2 class="pb-4 text-xl font-extrabold">QR code for your link:</h2>
+                    <canvas bind:this={canvas} />
+                </div>
             </TabPanel>
         </TabPanels>
     </TabGroup>
-
-    <!-- <Button variant="ghost" on:click={() => setPanel('import')} class="flex items-center gap-2"
-        ><FolderOpen />Open</Button
-    >
-    {#if $reflections.length}
-        <Button on:click={() => setPanel('export')} variant="ghost" class="flex items-center gap-2"
-            ><Download />Save</Button
-        >
-    {/if}
-    <div class="col-span-2">
-        {#if openPanel}
-            <div
-                class="bg-gray-50/5"
-                in:slide={{ axis: 'y', duration: 300 }}
-                out:slide={{ axis: 'y', duration: 300 }}
-            >
-                {#if openPanel === 'import'}
-                    <Button variant="ghost" on:click={openFile} class="flex items-center gap-2"
-                        ><FolderOpen />Open file</Button
-                    >
-                {:else if openPanel === 'export'}
-                    <Button
-                        on:click={() => saveFile($reflections)}
-                        variant="ghost"
-                        class="flex items-center gap-2"><Download />Save</Button
-                    >
-                {/if}
-            </div>
-        {/if}
-    </div> -->
 </div>
