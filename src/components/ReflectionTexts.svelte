46754050d7281465afd848adbd49158b88fe2ff7
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-04-17 07:06:12 +0200

    Add icons to reflection

--- src/components/ReflectionTexts.svelte
+++ src/components/ReflectionTexts.svelte
@@ -5,15 +5,26 @@
 </script>
 
 <script lang="ts">
+    import { colors, icons } from '$lib/constants'
+
     export let reflectionStep: Readable<ReflectionStep>
 </script>
 
 <h2
     class={cx(
-        'text-center text-lg font-normal normal-case 2xs:text-2xl xs:text-3xl',
+        'flex items-center justify-center gap-2 text-center text-lg font-normal normal-case 2xs:text-2xl xs:text-3xl',
         isLifewheelStep($reflectionStep) ? $reflectionStep.colors.text : 'text-emerald-400',
     )}
 >
+    {#if isLifewheelStep($reflectionStep)}
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class={cx(colors[$reflectionStep.i].text, '-mb-0.5')}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"><path fill="currentColor" d={icons[$reflectionStep.i]} /></svg
+        >
+    {/if}
     {$reflectionStep.title}
 </h2>
 <p class="whitespace-pre-wrap pt-2 text-sm 2xs:text-base xs:text-lg">
