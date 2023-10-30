cdd92f20db1bff17a76699d6df2dfda2a9f5741e
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-10-30 16:47:59 +0100

    Add regular link import

--- src/components/LinkImport.svelte
+++ src/components/LinkImport.svelte
@@ -12,30 +12,19 @@
 
     let payload: ParsedLink
 
-    onMount(() => {
+    onMount(async () => {
         if (window.location.hash) {
             try {
                 payload = CURRENT_PROTOCOL.parseLink(window.location.hash.slice(1))
 
                 if (!payload.encrypted && payload.data) {
-                    // TODO: CURRENT_PROTOCOL.importLink()
-                    // TODO: keep unique entries and update app state
-                    $reflections = importUniqueEntries(
-                        $reflections,
-                        payload.data,
-                        payload.protocolVersion,
-                    )
+                    const importedEntries = CURRENT_PROTOCOL.importLink(payload)
+                    $reflections = CURRENT_PROTOCOL.getUniqueEntries($reflections, importedEntries)
+
                     closeLinkImport()
                 }
 
                 // IDEA: Maybe try importing encrypted links with the saved key, and then fallback to the password form if it doesn't work.
-
-                // TODO: Edge case: If you have data in the app, and a key stored: What should happen if you import another encrypted link (or file for that matter)?
-                // Should you enter the password to decrypt the new data, but keep your first password before you started importing?
-                // Or should it overwrite with the most recent key?
-                // Maybe we could solve this by letting the import key be a separate thing. Maybe we could have an "account/security" menu in the top right of the screen where you could set your ENCRYPTION key (which is persisted and then tried for imports, and primarily used for exports).
-                // Then we could have temporary import keys to combine data. This could also clean up the UI for the ManageData component.
-                // Maybe save all known decryption keys, if you import multiple files from different devices. Then we could automatically import from all known import keys. And always use the export key set in your "account" settings in the top right of the screen. A good icon could perhaps be the Lock (for encryption enabled) and unlocked (for unencrypted)
             } catch (error) {
                 console.error('Invalid link: ', error)
                 closeLinkImport()
