ac57702c193a8b7f52771a406b1f0b4b4c845167
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-02-11 21:28:38 +0100

    Upgrade deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
