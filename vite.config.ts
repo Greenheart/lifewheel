b805b348616947638beb48c618bc093982b853f3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-09-15 13:09:01 +0200

    Update deps and simplify setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import Icons from 'unplugin-icons/vite'
 import { setDefaultResultOrder } from 'dns'
 
 setDefaultResultOrder('verbatim')
 
 export default defineConfig({
-    plugins: [sveltekit(), basicSsl(), Icons({ compiler: 'svelte' })],
+    plugins: [sveltekit(), Icons({ compiler: 'svelte' })],
 })
