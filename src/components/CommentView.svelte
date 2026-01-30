cc9692f1b493f6120ac17b9c103bbb0522717001
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-01-30 08:17:55 +0100

    Use invisible class from tailwind

--- src/components/CommentView.svelte
+++ src/components/CommentView.svelte
@@ -11,7 +11,10 @@
     let { textData, hidden }: Props = $props()
 </script>
 
-<div style="border: 2px solid white; padding: 1em; border-radius: 0.5em; background-color: rgba(255, 255, 255, 0.1);" class={['', "comment-view", hidden ? 'hidden-comment' : ''].join(' ')}>
+<div
+    style="border: 2px solid white; padding: 1em; border-radius: 0.5em; background-color: rgba(255, 255, 255, 0.1);"
+    class={['', 'comment-view', hidden ? 'invisible' : ''].join(' ')}
+>
     <p>
         {textData}
     </p>
