903d1a88f9afdd5e3d42ab0f497be5dfc3b152b3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-01-30 14:22:16 +0100

    Log protocol version when importing for clarity

--- src/lib/protocols/v1/protocol.ts
+++ src/lib/protocols/v1/protocol.ts
@@ -106,7 +106,7 @@ const PROTOCOL: Protocol = {
         )
 
         console.log(
-            `Imported ${Math.abs(
+            `[v${PROTOCOL.PROTOCOL_VERSION}] Imported ${Math.abs(
                 updatedEntries.length - currentEntries.length,
             )} - filtered out ${Math.abs(newEntries.length - updatedEntries.length)}`,
             updatedEntries.map((e) => e.time.getTime()),
