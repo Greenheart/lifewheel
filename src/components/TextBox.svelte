19c2517d6ce96308e7c52894f81269423c5783fa
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-01-30 13:57:00 +0100

    Make textarea grow together with the content

--- src/components/TextBox.svelte
+++ src/components/TextBox.svelte
@@ -4,18 +4,52 @@
 
 <script lang="ts">
     type Props = {
-        commentState: CommentState
+        value: CommentState
     }
 
-    let { commentState = $bindable() }: Props = $props()
+    let { value = $bindable() }: Props = $props()
 </script>
 
-<textarea
-    class="comment-textarea min-h-25 w-full resize-none rounded-md p-2 text-white outline-1 outline-white"
-    rows="2"
-    cols="42"
-    maxlength="300"
-    placeholder="Write your reflection..."
-    bind:value={commentState}
->
-</textarea>
+<div class="autogrow">
+    <textarea
+        class="comment-textarea"
+        rows="2"
+        cols="42"
+        maxlength="300"
+        placeholder="Write your reflection..."
+        bind:value
+        oninput={(event) => {
+            ;(event.currentTarget.parentNode as HTMLElement).dataset.content =
+                event.currentTarget.value
+        }}
+    >
+    </textarea>
+</div>
+
+<style>
+    .autogrow {
+        display: grid;
+    }
+
+    .autogrow::after {
+        content: attr(data-content) ' ';
+        white-space: pre-wrap;
+        visibility: hidden;
+    }
+
+    .autogrow > textarea {
+        resize: none;
+        overflow: hidden;
+    }
+
+    .autogrow > textarea,
+    .autogrow::after {
+        min-height: 100px;
+        width: 100%;
+        border-radius: 6px;
+        color: white;
+        padding: 0.5rem;
+        outline: 1px solid white;
+        grid-area: 1 / 1 / 2 / 2;
+    }
+</style>
